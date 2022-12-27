005d125329ef05fb0b8a08bc59175e2a080189bb
HOWARD HUANG <howardhuang@college.harvard.edu>
2022-12-27 10:06:20 -0500

    edit

---
+++ main.js
@@ -0,0 +1,14 @@
+// write js for website that uses react and javascript to create a simple physics engine with blocks
+
+// import react and react dom
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// import css
+import './index.css';
+
+// import components
+import App from './App';
+
+// render app
+ReactDOM.render(<App />, document.getElementById('root'));
